824199d13883e0eed8349f831f2d816237da0986
luchomellu <luchomellu@live.com.ar>
2024-10-30 16:24:12 -0300

    mas avances (comiteo porque no comiteo hace mucho)

---
+++ src/components/DownloadCV.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from './DownloadCV.module.css';
+import circle from '../assets/svgs/circle-svgrepo-com.svg';
+import clear from '../assets/svgs/clear-svgrepo-com.svg';
+import cvES from '../assets/cvs/MellusoLucianoES.pdf';
+import cvEN from '../assets/cvs/MellusoLucianoEN.pdf';
+import downloadSVG from '../assets/svgs/file-download.svg'
+
+const DownloadCV = () => {
+  return (
+    <div className={styles.downloadSection}>
+        <div className={styles.barraDW}>
+            <div className={styles.iconosDW}>
+                <img src={circle} alt="" />
+                <img src={circle} alt="" />
+                <span>
+                    <img src={clear} alt="" />
+                </span>
+            </div>
+        </div>
+        <div className={styles.contenidoDW}>
+            <h2>Descarga mi curriculum!</h2>
+            <div className={styles.cvs}>
+                <a href={cvES} download="MellusoLucianoCV-ES.pdf">
+                    <img src={downloadSVG} alt="" />
+                    <h3>Español</h3>
+                </a>
+                <a href={cvEN} download="MellusoLucianoCV-EN.pdf">
+                    <img src={downloadSVG} alt="" />
+                    <h3>Ingles</h3>
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DownloadCV
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,11 +4,13 @@ import GitHubSection from './GitHubSection';
 import myPicture from '../assets/images/pic.jpg';
 import gitLogo from '../assets/svgs/github-svgrepo-com.svg';
 import linkedinLogo from '../assets/svgs/linkedin-svgrepo-com.svg';
+import DownloadCV from './DownloadCV';
+import stilo from './Main.module.css';
 
 const Main = () => {
   const presentationRef = useRef(null);
   const isMounted = useRef(false); // Variable para comprobar si ya se ejecutó
-
+  
   useEffect(() => {
     if (!isMounted.current) {
       isMounted.current = true; // Marca como "ya montado" después de la primera ejecución
@@ -21,30 +23,36 @@ const Main = () => {
       }
     }
   }, []); // El efecto se ejecuta una vez al montar el componente
-
+  
   return (
     <main>
-      <h1 className='presentation' ref={presentationRef}></h1>
-      <div className='socialsandpic'>
-        <div className='socials'>
-          <h2>Seguime en mis redes!</h2>
-          <div className='links'>
-            <a href="https://github.com/luchomellu">
-              <img src={gitLogo} alt="" className='gitLogo'/>
-            </a>
-            <a href="https://www.linkedin.com/in/luchomellu/">
-              <img src={linkedinLogo} alt="" className='linkedinLogo'/>
-            </a>
+      <div className='aboutMe'>
+        <div className={stilo.textoide}>
+          <h1 ref={presentationRef} className={stilo.presentation}></h1>
+          <h1 className={stilo.invisible}>Hola! Mi nombre es Luciano Melluso y soy desarrollador de software!</h1>
+        </div>
+        <div className='socialsandpic'>
+          <div className='socials'>
+            <h2>Seguime en mis redes!</h2>
+            <div className='links'>
+              <a href="https://github.com/luchomellu">
+                <img src={gitLogo} alt="" className='gitLogo'/>
+              </a>
+              <a href="https://www.linkedin.com/in/luchomellu/">
+                <img src={linkedinLogo} alt="" className='linkedinLogo'/>
+              </a>
+            </div>
+          </div>
+          <div className='polaroid'>
+            <img src={myPicture} alt="" className='pic'/>
           </div>
         </div>
-        <div className='polaroid'>
-          <img src={myPicture} alt="" className='pic'/>
+        <div className='text-bubble'>
+          <p>Soy Luciano Melluso, estudiante de la carrera de Tecnicatura Universitaria en Desarrollo de Aplicaciones Informáticas. Actualmente resido en Tandil y me encuentro en la etapa final de mi carrera, con solo cuatro exámenes finales pendientes para graduarme. Estoy buscando oportunidades laborales o programas de bootcamp que me permitan aplicar y expandir mis conocimientos y habilidades en el desarrollo de aplicaciones informáticas, mientras concluyo mis estudios.</p>
         </div>
       </div>
-      <div className='text-bubble'>
-        <p>Soy Luciano Melluso, estudiante de la carrera de Tecnicatura Universitaria en Desarrollo de Aplicaciones Informáticas. Actualmente resido en Tandil y me encuentro en la etapa final de mi carrera, con solo cuatro exámenes finales pendientes para graduarme. Estoy buscando oportunidades laborales o programas de bootcamp que me permitan aplicar y expandir mis conocimientos y habilidades en el desarrollo de aplicaciones informáticas, mientras concluyo mis estudios.</p>
-      </div>
       <GitHubSection/>
+      <DownloadCV/>
     </main>
   );
 }
