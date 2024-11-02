5eb308a35d614cce82477648db04f4d644b816b2
luchomellu <luchomellu@live.com.ar>
2024-11-02 01:17:19 -0300

    seccion de descarga con nuevo disenio a terminar

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -4,12 +4,14 @@ import circle from '../assets/svgs/circle-svgrepo-com.svg';
 import clear from '../assets/svgs/clear-svgrepo-com.svg';
 import cvES from '../assets/cvs/MellusoLucianoES.pdf';
 import cvEN from '../assets/cvs/MellusoLucianoEN.pdf';
-import downloadSVG from '../assets/svgs/file-download.svg'
+import pdf from '../assets/svgs/pdf.svg'
+import gif from '../assets/svgs/gif.svg'
 
 const DownloadCV = () => {
   return (
     <div className={styles.downloadSection}>
         <div className={styles.barraDW}>
+            <p>---- descarga.zip ----</p>
             <div className={styles.iconosDW}>
                 <img src={circle} alt="" />
                 <img src={circle} alt="" />
@@ -19,9 +21,11 @@ const DownloadCV = () => {
             </div>
         </div>
         <div className={styles.contenidoDW}>
+            {/*
             <h2>Descarga mi curriculum!</h2>
             <iframe src="https://giphy.com/embed/MxYQrB9jeGzza"></iframe>  
             <div className={styles.cvs}>
+                
                 <a href={cvES} download="MellusoLucianoCV-ES.pdf">
                     <img src={downloadSVG} alt="" />
                     <h3>Español</h3>
@@ -30,7 +34,22 @@ const DownloadCV = () => {
                     <img src={downloadSVG} alt="" />
                     <h3>Ingles</h3>
                 </a>
+                
+
             </div>
+            */}
+            <a href="">
+                <img src={pdf} alt="" />
+                <p>MellusoLucianoCV-ES.pdf</p>
+            </a>
+            <a href="">
+                <img src={pdf} alt="" />
+                <p>MellusoLucianoCV-EN.pdf</p>
+            </a>
+            <a href="">
+                <img src={gif} alt="" />
+                <p>jijiji.pdf</p>
+            </a>
         </div>
     </div>
   )
